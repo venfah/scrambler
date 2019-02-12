fae920fea10324c86607e787cbaf24cc683701f6
venfah <32269227+venfah@users.noreply.github.com>
2019-02-12 16:35:57 +0530

    Create find_max_three.py

---
+++ python/examples/find_max_three.py
@@ -0,0 +1,23 @@
+## To sort based on on of words in a text file.
+filename = '../resources/content.txt'
+count_dict = {}
+
+for line in open(filename, 'r'):
+	words = line.split()
+	for word in words:
+		if word in count_dict:
+			count_dict[word] += 1
+		else:
+			count_dict[word]  = 1
+		
+def on_values(value):
+	return value[1]
+sorted_list = sorted(count_dict.items(), reverse=True,  key=on_values)
+
+max = dict()
+
+for each in sorted_list:
+	max[each[1]] = each[1]
+	if len(max) <= 3:
+		print (str(each[0]) + " : " +  str(each[1]))
+
