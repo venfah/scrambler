ed1778c2a82d533588074c95d260143b978781b6
Venfah <32269227+venfah@users.noreply.github.com>
2019-05-28 14:22:06 +0530

    Updated the code to simplify

--- python/vexata/list_sparse.py
+++ python/vexata/list_sparse.py
@@ -1,26 +1,22 @@
 
-
-list_item = '''
+list_item = ''' 
 name available recieved comment
 service1 1 2 comment
 service2 4 5 comment again
 '''
 
 result_dict = dict()
-header = []
 
 for enu, line in enumerate(list_item.splitlines()):
-    temp = []
     if enu == 0:
         continue
     if enu == 1:
-        header.extend(line.split())
+        header = line.split()
     else:
-        temp.extend(line.split(' ', 3))
+        temp = line.split(' ', 3)
         result_dict[temp[0]] = dict()
         for i in range(0, len(temp)):
             result_dict[temp[0]][header[i]] = temp[i]
-
 
 print (result_dict)
 
