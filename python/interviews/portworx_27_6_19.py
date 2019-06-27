ba6096dfdce5b71ff8a8ceb91f292b755f678432
Venfah <32269227+venfah@users.noreply.github.com>
2019-06-27 19:02:48 +0530

    Update portworx_27_6_19.py

--- python/interviews/portworx_27_6_19.py
+++ python/interviews/portworx_27_6_19.py
@@ -1,6 +1,40 @@
+'''
+1. find prime no from 1 to 100
 
 '''
-find the equilibrium of the list
+
+
+def find_prime(no):
+    if no == 1 or no == 2:
+        return True
+    for i in range(2, no):
+        if divmod(no, i)[1] == 0:
+            return False
+    return True
+
+for i in range (1, 101):
+    if find_prime(i) == True:
+        print (i),
+'''
+OUTPUT
+Guest ran 18 lines of Python 2 (finished in 1.56s):
+
+1 2 3 5 7 11 13 17 19 23 29 31 37 41 43 47 53 59 61 67 71 73 79 83 89 97
+>>> 
+'''
+
+
+'''
+2. 
+'''
+
+'''
+OUTPUT
+
+'''
+
+'''
+3 find the equilibrium of the list
 ie find an element such that right and left side of the element's sum is same (excluding the element)
 
 '''
@@ -20,7 +54,7 @@ for ind in range(1, len(l1)-1):
         break    
 else:
     if Found == False:
-        print ("equi not found")
+        print ("equilibrium of list: not found")
         
         
 '''
@@ -30,3 +64,20 @@ OUTPUT
 ([4, 1, 2, 3], [6, 4], 10, 10)
 
 ''''
+
+'''
+Questions on linux and storage:
+
+1. how to get the lun details, fdisk -l, multipath -ll, pvdisplay, lddisply, lvdisplay
+2. how to create lv - pvcreate, ldcreate, lvcreate
+3. how to list pcie devices, lspci
+4. how to get pwwn no of hba
+systool -c fc_host -v | grep -i wwn
+5. how to test a raw lun with io
+   differetn randdom ness of the IO, read write perc, zone set, zone create, different bs, combination of bs, DIRECT and SYNC flags
+   trim, writesame zero and different patterns, read modified writes
+   
+6. how to get the iostat in linux, iostat.
+7. what is inflight IO
+8. What is loop back device
+'''
